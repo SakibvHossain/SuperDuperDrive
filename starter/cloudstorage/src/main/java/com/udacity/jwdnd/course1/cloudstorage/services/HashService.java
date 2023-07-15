46b3babc463df7ff09f7b8b12772b38a74b079f6
XSakibxHossainX <sakibxhossian021@gmail.com>
2023-07-15 20:26:56 +0600

    done

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/HashService.java
@@ -0,0 +1,35 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+
+@Service
+public class HashService {
+
+    public final Logger logger = LoggerFactory.getLogger(HashService.class);
+
+    public String getHashedValue(String data, String salt) {
+        byte[] hashedValue = null;
+
+        int iterCount = 12288;
+        int derivedKeyLength = 256;
+        KeySpec spec = new PBEKeySpec(data.toCharArray(), salt.getBytes(), iterCount, derivedKeyLength * 8);
+        try {
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            hashedValue = factory.generateSecret(spec).getEncoded();
+        } catch (InvalidKeySpecException | NoSuchAlgorithmException e) {
+            logger.error(e.getMessage());
+        }
+
+        return Base64.getEncoder().encodeToString(hashedValue);
+    }
+
+}
