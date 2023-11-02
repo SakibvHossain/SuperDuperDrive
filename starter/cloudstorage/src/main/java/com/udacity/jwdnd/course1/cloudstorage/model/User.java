e3d7a891d9258acf1dedb53755bd627540b24b7a
SakibvHossain <sakibxhossian021@gmail.com>
2023-11-02 21:12:26 +0600

    feat: Updated user

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/User.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/User.java
@@ -8,7 +8,7 @@ public class User {
     private String firstName;
     private String lastName;
 
-    public User(Integer userId, String username, String salt, String password, String firstName, String lastName){
+    public User(Integer userId, String username, String salt, String password, String firstName, String lastName) {
         this.userId = userId;
         this.username = username;
         this.salt = salt;
@@ -17,7 +17,7 @@ public class User {
         this.lastName = lastName;
     }
 
-    public Integer getUserId(){
+    public Integer getUserId() {
         return userId;
     }
 
@@ -25,19 +25,27 @@ public class User {
         this.userId = userId;
     }
 
-    public String getUsername(){
+    public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username){
+    public void setUsername(String username) {
         this.username = username;
     }
 
-    public String getPassword(){
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password){
+    public void setPassword(String password) {
         this.password = password;
     }
 
@@ -45,11 +53,15 @@ public class User {
         return firstName;
     }
 
-    public String getLastname() {
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
         return lastName;
     }
 
-    public String getSalt() {
-        return salt;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/UserService.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/UserService.java
@@ -18,21 +18,24 @@ public class UserService {
         this.hashService = hashService;
     }
 
+    public boolean isUsernameAvailable(String username) {
+        return userMapper.getUser(username) == null;
+    }
+
     public int createUser(User user) {
         SecureRandom random = new SecureRandom();
         byte[] salt = new byte[16];
         random.nextBytes(salt);
         String encodedSalt = Base64.getEncoder().encodeToString(salt);
         String hashedPassword = hashService.getHashedValue(user.getPassword(), encodedSalt);
-        return userMapper.insert(new User(null, user.getUsername(), encodedSalt, hashedPassword, user.getFirstName(), user.getLastname()));
-    }
-
-    public boolean isUsernameAvailable(String username) {
-        return userMapper.getUser(username) == null;
+        return userMapper.insert(new User(null, user.getUsername(), encodedSalt, hashedPassword, user.getFirstName(), user.getLastName()));
     }
 
     public User getUser(String username) {
         return userMapper.getUser(username);
     }
 
+    public User getUser(Integer userId) {
+        return userMapper.getUserById(userId);
+    }
 }
