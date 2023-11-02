1c30c120d2fb4f92fa6cc05cfa985d94ce56ca13
SakibvHossain <sakibxhossian021@gmail.com>
2023-11-02 21:17:21 +0600

    feat: Updated login related files

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/LoginController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/LoginController.java
@@ -3,14 +3,13 @@ package com.udacity.jwdnd.course1.cloudstorage.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
-    @GetMapping
-    public String getLoginPage(@RequestParam(value = "error",required = false) String error,
-                               @RequestParam(value = "logout",	required = false) String logout){
+
+    @GetMapping()
+    public String loginView() {
         return "login";
     }
 }
--- starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/LoginPage.java
+++ starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/LoginPage.java
@@ -1,38 +1,37 @@
 package com.udacity.jwdnd.course1.cloudstorage;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage {
-    WebDriverWait wait;
-
     @FindBy(id = "inputUsername")
-    WebElement userName;
+    private WebElement inputUserName;
+
     @FindBy(id = "inputPassword")
-    WebElement password;
-    @FindBy(id = "login-button")
-    WebElement login_button;
+    private WebElement inputPassword;
+
+    @FindBy(id = "submit-button")
+    private WebElement submitButton;
+
+    private final JavascriptExecutor js;
 
     public LoginPage(WebDriver driver) {
-        PageFactory.initElements(driver,this);
-        wait = new WebDriverWait(driver,2);
+        PageFactory.initElements(driver, this);
+        js = (JavascriptExecutor) driver;
     }
 
-    public void login(String username, String passWord){
-        //Username
-        wait.until(ExpectedConditions.visibilityOf(userName));
-        userName.click();
-        userName.sendKeys(username);
-        //Password
-        wait.until(ExpectedConditions.visibilityOf(password));
-        password.click();
-        password.sendKeys(passWord);
-        //Submit
-        wait.until(ExpectedConditions.visibilityOf(login_button));
-        login_button.click();
+    public void setUserName(String userName) {
+        js.executeScript("arguments[0].value='"+ userName +"';", inputUserName);
+    }
+
+    public void setPassword(String password) {
+        js.executeScript("arguments[0].value='"+ password +"';", inputPassword);
+    }
+
+    public void login() {
+        js.executeScript("arguments[0].click();", submitButton);
     }
 }
